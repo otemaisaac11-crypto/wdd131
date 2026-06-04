63cc6c2201a106b24dc49903604bd37dd2c947cd
Otema Isaac <otemaisaac11@gmail.com>
2026-06-04 14:05:30 +0300

    date

--- week02/script/temple.js
+++ week02/script/temple.js
@@ -1,16 +1,18 @@
 
 
-const year = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", function() {
+    const year = new Date().getFullYear();
 
-// Place current year in footer
-document.getElementById("currentyear").textContent = year;
+    // Place current year in footer
+    document.getElementById("currentyear").textContent = year;
 
-// Place last modified date in footer
-document.getElementById("lastModified").textContent =
-    `Last Modified: ${document.lastModified}`;
-    
-// . Toggle the 'show' class on the nav and button when clicked
-document.getElementById("menu").addEventListener("click", function() {
-    document.querySelector("nav").classList.toggle("show");
-    this.classList.toggle("show");
+    // Place last modified date in footer
+    document.getElementById("lastModified").textContent =
+        `Last Modified: ${document.lastModified}`;
+        
+    // Toggle the 'show' class on the nav and button when clicked
+    document.getElementById("menu").addEventListener("click", function() {
+        document.querySelector("nav").classList.toggle("show");
+        this.classList.toggle("show");
+    });
 });
