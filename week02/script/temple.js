de7eff5a91238fa0cbeaf97031371e74189774bf
Otema Isaac <otemaisaac11@gmail.com>
2026-06-04 13:54:51 +0300

    final steps

--- week02/script/temple.js
+++ week02/script/temple.js
@@ -1,7 +1,14 @@
 
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
 
+const year = new Date().getFullYear();
+
+// Place current year in footer
+document.getElementById("currentyear").textContent = year;
+
+// Place last modified date in footer
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
+    
 // . Toggle the 'show' class on the nav and button when clicked
 document.getElementById("menu").addEventListener("click", function() {
     document.querySelector("nav").classList.toggle("show");
