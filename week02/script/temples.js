bd0446a8ad0baba8fd442520380706fb78dd4449
Otema Isaac <otemaisaac11@gmail.com>
2026-06-04 14:19:49 +0300

    finish up

--- week02/script/temples.js
+++ week02/script/temples.js
@@ -1,18 +1,11 @@
+// Place current year in footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    const year = new Date().getFullYear();
-
-    // Place current year in footer
-    document.getElementById("currentyear").textContent = year;
-
-    // Place last modified date in footer
-    document.getElementById("lastModified").textContent =
-        `Last Modified: ${document.lastModified}`;
-        
-    // Toggle the 'show' class on the nav and button when clicked
-    document.getElementById("menu").addEventListener("click", function() {
-        document.querySelector("nav").classList.toggle("show");
-        this.classList.toggle("show");
-    });
+// Place last modified date in footer
+document.getElementById("lastModified").textContent = document.lastModified;
+    
+// Toggle the 'show' class on the nav and button when clicked
+document.getElementById("menu").addEventListener("click", function() {
+    document.querySelector("nav").classList.toggle("show");
+    this.classList.toggle("show");
 });
